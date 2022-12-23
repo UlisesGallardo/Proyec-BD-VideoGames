ebf08c12a2b8c2968a4d13fd0cc6a3dd6665582b
Ulises <89113180+UlisesGallardo@users.noreply.github.com>
2022-12-23 02:06:02 -0600

    Update db.js

--- backend/api/config/db.js
+++ backend/api/config/db.js
@@ -2,7 +2,6 @@ import mysql from "mysql2";
 import dotenv from "dotenv";
 
 dotenv.config();
-//const connection = mysql.createConnection('mysql://87crf9p9jb2fe8hal4vs:pscale_pw_poeYkQ1v3kY4UCPEDyKeUwy2xXKtxasKwhIQXvQ79wN@us-east.connect.psdb.cloud/videogames?ssl={"rejectUnauthorized":true}');
 const connection = mysql.createConnection({uri:process.env.DATABASE_URL, multipleStatements: true});
 export default connection.promise();
 
