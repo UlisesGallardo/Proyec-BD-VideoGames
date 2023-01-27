93ed1a93260e7ec1e8239713fc440e7b2b11de96
Ulises <89113180+UlisesGallardo@users.noreply.github.com>
2023-01-27 14:55:11 -0600

    Update HomePage.jsx

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -18,29 +18,6 @@ function HomePage() {
         navigate('/search');
     }
 
-    
-    /*
-    useEffect(()=>{
-
-        axios({
-            url: "https://api.igdb.com/v4/games",
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Client-ID': 'den4dtbz6v6fpyv7pi9zs2anp1ivpl',
-                'Authorization': 'Bearer fm709n7tam6nsqlk8wvsyqmnv85kq2',
-        },
-        data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,checksum,collection,cover,created_at,dlcs,expanded_games,expansions,external_games,first_release_date,follows,forks,franchise,franchises,game_engines,game_localizations,game_modes,genres,hypes,involved_companies,keywords,language_supports,multiplayer_modes,name,parent_game,platforms,player_perspectives,ports,rating,rating_count,release_dates,remakes,remasters,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-        })
-        .then(response => {
-            console.log(response.data);
-        })
-        .catch(err => {
-            console.error(err);
-        });
-    
-    },[])*/
-
     return (
         <div>
             <Container>
